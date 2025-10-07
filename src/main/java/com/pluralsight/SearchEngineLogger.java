b8a7914b362915aad77074f3c20373e1472da961
Stephen_A <ankustephen99@gmail.com>
2025-10-07 15:27:32 -0400

    Created method to launch application

--- src/main/java/com/pluralsight/SearchEngineLogger.java
+++ src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class SearchEngineLogger {
+    public static void main(String[] args) {
+        String fileName = "logs.txt";
+        Scanner scanner = new Scanner(System.in);
+        boolean isDone = false;
+        ZonedDateTime currentTime = ZonedDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-dd-MM HH:mm:ssa z");
+
+
+        while (!isDone) {
+            System.out.println("Press 1 to Launch Application");
+            int command = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (command) {
+                case 1:
+                    launchApplication(currentTime.format(myFormatObj), fileName);
+                    int command1 = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (command1) {
+                        case 1:
+                            System.out.println("hi");
+                    }
+                    break;
+            }
+        }
+    }
+    public static void launchApplication(String dateTime, String fileName){
+        actionLogger(dateTime, "launch", fileName);
+    }
+
+    public static void actionLogger(String dateTime, String action, String fileName){
+
+    }
 }
