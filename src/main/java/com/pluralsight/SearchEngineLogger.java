ceb29f115f43898565f5ce3204adf95a2ee4585b
Stephen_A <ankustephen99@gmail.com>
2025-10-07 15:41:48 -0400

    Created methods to search and exit application

--- src/main/java/com/pluralsight/SearchEngineLogger.java
+++ src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -21,11 +21,22 @@ public class SearchEngineLogger {
             switch (command) {
                 case 1:
                     launchApplication(currentTime.format(myFormatObj), fileName);
+                    System.out.println("Please select your next action:");
+                    System.out.println("1. Search");
+                    System.out.println("2. Exit the Application");
                     int command1 = scanner.nextInt();
                     scanner.nextLine();
                     switch (command1) {
                         case 1:
-                            System.out.println("hi");
+                            System.out.println("Enter your search query here:");
+                            String userQuery = scanner.nextLine();
+                            searchApplication(currentTime.format(myFormatObj), fileName, userQuery);
+                            break;
+                        case 2:
+                            System.out.println("Thank you for trying this application!");
+                            exitApplication(currentTime.format(myFormatObj), fileName);
+                            isDone = true;
+                            break;
                     }
                     break;
             }
@@ -35,7 +46,17 @@ public class SearchEngineLogger {
         actionLogger(dateTime, "launch", fileName);
     }
 
-    public static void actionLogger(String dateTime, String action, String fileName){
+    public static void searchApplication(String dateTime, String fileName, String search){
+        actionLogger(dateTime, "search", fileName, search);
+    }
 
+    public static void exitApplication(String dateTime, String fileName){
+        actionLogger(dateTime, "exit", fileName);
+    }
+
+    public static void actionLogger(String dateTime, String action, String fileName){
+    }
+
+    public static void actionLogger(String dateTime, String action, String fileName, String search){
     }
 }
